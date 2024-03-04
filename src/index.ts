60473e9483f461f90ebbf4bef1f98c3e53cd0f28
Yeon Vinzenz Varapragasam <YeonVinzenz.Varapragasam@trustedshops.de>
2024-03-04 12:10:25 +0100

    0.5.38

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import type { UDPPlugin } from './definitions';
 
 const UDP = registerPlugin<UDPPlugin>('UDP', {
   web: () => import('./web').then(m => new m.UDPWeb()),
+  electron: () => (window as any).CapacitorCustomPlatform.plugins.UDPElectron
 });
 
 export * from './definitions';
